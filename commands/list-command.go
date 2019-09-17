5d52fcaa58f48f28f6aadd7f65a8938004772daf
Ke Huang <kehuang@teamyi.com>
2019-09-17 16:38:17 +0800

    chore: add sorting and filter to list command

--- commands/list-command.go
+++ commands/list-command.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"errors"
 	"reflect"
+	"sort"
 	"strings"
 
 	"github.com/hkbarton/arango-cli/state"
@@ -14,19 +15,23 @@ type objWithName interface {
 	Name() string
 }
 type objGetter func() ([]objWithName, error)
+type stringFilter func(input string) bool
 
-func getObjectNames(objGetter objGetter) ([]string, error) {
+func getObjectNames(objGetter objGetter, filter stringFilter) ([]string, error) {
 	objs, err := objGetter()
 	if err != nil {
 		return nil, err
 	}
 	names := make([]string, 0, len(objs))
 	for _, obj := range objs {
-		name := obj.Name()
-		if len(strings.TrimSpace(name)) > 0 {
+		name := strings.TrimSpace(obj.Name())
+		if len(name) > 0 && (filter == nil || filter(name)) {
 			names = append(names, name)
 		}
 	}
+	sort.Slice(names, func(i, j int) bool {
+		return names[i] < names[j]
+	})
 	return names, nil
 }
 
@@ -40,6 +45,10 @@ func convertObjGetterResult(objs interface{}, err error) ([]objWithName, error) 
 	return result, err
 }
 
+func systemNameFilter(name string) bool {
+	return !strings.HasPrefix(name, "_")
+}
+
 func (r ListCommandRunner) Run(c *Command, resultChan chan interface{}) {
 	defer close(resultChan)
 	if c.Args == nil || len(c.Args) < 1 {
@@ -49,15 +58,20 @@ func (r ListCommandRunner) Run(c *Command, resultChan chan interface{}) {
 	var err error
 	var result interface{}
 	target := c.Args[0]
+	_, willShowAll := c.Options["a"]
+	var filter stringFilter
+	if !willShowAll {
+		filter = systemNameFilter
+	}
 	switch target {
 	case "db":
 		result, err = getObjectNames(func() ([]objWithName, error) {
 			return convertObjGetterResult(state.DBClient().Databases(nil))
-		})
+		}, filter)
 	case "coll":
 		result, err = getObjectNames(func() ([]objWithName, error) {
 			return convertObjGetterResult(state.CurrentDB().Collections(nil))
-		})
+		}, filter)
 	default:
 		err = errors.New("Unknown list target " + target)
 	}
